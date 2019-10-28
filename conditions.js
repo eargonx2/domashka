30ac4566f275f38a26bc078acfbffe6c577aab57
Ykov <nevidomskiy8@gmail.com>
2019-10-28 20:06:01 +0200

    First commit

---
+++ conditions.js
@@ -0,0 +1,54 @@
+var msg = 'valid';
+var notification = document.querySelector('.notification');
+
+var blockClass = msg === 'valid' ? 'valid' : 'error';
+// if (msg === 'valid') {
+//    blockClass = 'confirm';
+// } else {
+//   blockClass = 'error';
+// };
+
+
+// var INFO = 'INFO',
+//       WARNING = 'WARN',
+//       ERROR = 'ERR',
+//       CONFIRM = 'CONFIRM';
+
+// if (msg === INFO) {
+// notification.classList.add('info');
+// notification.innerText = 'info';
+// }
+// else if (msg === WARNING) {
+// notification.classList.add('warning');
+// notification.innerText = 'warning';
+// }
+// else if (msg === ERROR) {
+// notification.classList.add('error');
+// notification.innerText = 'error';
+// }
+// else if (msg === confirmed) {
+// notification.classList.add('confirm');
+// notification.innerText = 'confirm';
+// } else {
+// notification.innerText = 'default log';
+// }
+
+// switch (msg) {
+  
+// case WARNING: // else if(msg === WARNING)
+//     notification.classList.add('warning');
+//     notification.innerText = 'warning';
+//     break;
+// case ERROR:   // else if(msg === ERROR)
+// notification.classList.add('error');
+// notification.innerText = 'error';
+// break;
+// case INFO:
+// case CONFIRM: // else if(msg === INFO || msg === CONFIRM)
+// notification.classList.add('confirm');
+// notification.innerText = 'confirm';
+// break;
+// default:
+// notification.innerText = 'default log';
+// break;
+// }
