4a21387007665b535b2cb8d1c1aabcebff958349
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-10-30 21:03:32 +0200

    Create srednee_arif.js

---
+++ lesson 2/srednee_arif.js
@@ -0,0 +1,9 @@
+var chislo= prompt('Введите первое число', '');
+var num1 = Number(chislo);
+let chislo2 = prompt('Введите второе число', '');
+var num2 = Number(chislo2);
+let chislo3 = prompt('Введите третье число', '');
+var num3 = Number(chislo3);
+
+var m = ((num1+num2+num3)/3);
+alert( 'Среднее арифметическое = '+m.toFixed(0)); 
