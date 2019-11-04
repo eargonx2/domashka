d79440eab4388cc68048fbfb2e5ae8b1a3988767
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-04 15:33:25 +0200

    Update random number while.js
    added cancel button

--- lesson 6/random number while.js
+++ lesson 6/random number while.js
@@ -1,6 +1,6 @@
-var num = prompt('Угадайте число от 1 до 10');
-var b =  Math.ceil(Math.random()*10);
-while (num == null) {
+var num = prompt('Угадайте число от 1 до 4');
+var b =  Math.ceil(Math.random()*4);
+while (num === null) {
 return;
 }
 while (b == num) {
@@ -8,5 +8,9 @@ alert(`Вы угадали, загаданное число ` + b);
 break;}
 while (b != num) {
 alert(`Вы не угадали, загаданое число ` +b);
-break;
+num = prompt('Угадайте число от 1 до 4');
+b =  Math.ceil(Math.random()*4);
+if (num == null) {
+return;
+}
 }
