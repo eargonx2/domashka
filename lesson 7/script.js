6cecb6325cf411be586f9d5f6790d630df4245ee
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-07 15:09:17 +0200

    Update script.js

--- lesson 7/script.js
+++ lesson 7/script.js
@@ -1,5 +1,5 @@
-﻿var wordsAmount = +prompt('Введите количество рандомных значений, которые будут сгенерированы');
-	  minCharacter = +prompt('Минимальное количество символов');
+var wordsAmount = +prompt('Введите количество рандомных значений, которые будут сгенерированы');
+    minCharacter = +prompt('Минимальное количество символов');
     maxCharacter = +prompt('Максимальное количество символов до 7');
     maxCharacter++;     //увеличиваем на 1 т.к. length считает с 0
 
@@ -29,7 +29,7 @@ for (var key in length) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 1 characters");}          //выводим пользователю слова с 1 символом и т.д.
+console.log(counter+" words with 1 characters");}          //выводим пользователю слова с 1 символом и т.д.
 
 var length2 = mass.filter(word => word.length == 2);
 
@@ -39,7 +39,7 @@ for (var key in length2) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 2 characters");}
+console.log(counter+" words with 2 characters");}
 
 var length3 = mass.filter(word => word.length == 3);
 
@@ -49,7 +49,7 @@ for (var key in length3) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 3 characters");}
+console.log(counter+" words with 3 characters");}
 
 var length4 = mass.filter(word => word.length == 4);
 
@@ -59,7 +59,7 @@ for (var key in length4) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 4 characters");}
+console.log(counter+" words with 4 characters");}
 
 var length5 = mass.filter(word => word.length == 5);
 
@@ -69,7 +69,7 @@ for (var key in length5) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 5 characters");}
+console.log(counter+" words with 5 characters");}
 
 var length6 = mass.filter(word => word.length == 6);
 
@@ -79,7 +79,7 @@ for (var key in length6) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 6 characters");}
+console.log(counter+" words with 6 characters");}
 
 var length7 = mass.filter(word => word.length == 7);
 
@@ -89,7 +89,7 @@ for (var key in length7) {
   counter++;
 }
 if (counter!=0){
-alert(counter+" words with 7 characters");}
+console.log(counter+" words with 7 characters");}
 
 
 var sortMass = [];
