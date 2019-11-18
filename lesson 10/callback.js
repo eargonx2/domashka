69fa2dfb5630e5d2c2b29711d7c4f80c3d7a01c9
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-18 14:42:03 +0200

    Update callback.js

--- lesson 10/callback.js
+++ lesson 10/callback.js
@@ -1,3 +1,27 @@
+var runner = {
+    $$runnerId: null,
+    $$tasks: [],
+    setSpeed: function (speed) {
+        this.stop();
+        this.start(speed);
+    },
+    add: function (task) {
+        this.$$tasks.push(task);
+    },
+    start: function (speed) {
+        var _this = this;
+        this.$$runnerId = setTimeout(function callback() {
+            _this.$$tasks.forEach(function () {
+                // Here
+            })
+            _this.$$runnerId = setTimeout(callback, speed);
+        }, speed);
+    },
+    stop: function () {
+        clearTimeout(this.$$runnerId);
+    }
+}
+
 runner.$$tasks = function first(){
 setInterval( function(){
 console.log('Hello');
