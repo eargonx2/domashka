970e06b7b5f504b708cb64dad3da2e012be830ee
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-21 08:59:15 +0200

    Update some.js

--- lesson 11/some.js
+++ lesson 11/some.js
@@ -1,12 +1,16 @@
 var arr = [1, -1, 2, -2, 3];
 
 function some(arr, cb) {
-	if (!cb) return;
-	for (var i=0; i < arr.length; i++) {
-		if(!cb(arr[i], i, arr)) {
-			return false;
-		}
-	}
-	return true;
+  if (!cb) return;
+  for (var i=0; i < arr.length; i++) {
+    if(cb(arr[i], i, arr)) {
+      return true;
+    }
+  }
+  return false;
 }
 
+
+console.log(some(arr, function (el) {
+return el == 1;
+}));
