cdaa4bdbf62973477f89ccc6c56862f54dfaa32f
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-22 15:55:40 +0200

    Update script.js

--- lesson 7/script.js
+++ lesson 7/script.js
@@ -1,98 +1,35 @@
 var wordsAmount = +prompt('Введите количество рандомных значений, которые будут сгенерированы');
     minCharacter = +prompt('Минимальное количество символов');
     maxCharacter = +prompt('Максимальное количество символов до 7');
-    maxCharacter++;     //увеличиваем на 1 т.к. length считает с 0
+        
 
-
-function generate(length) {              //генерируем случайные символы
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   for ( var i = 1; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
-   }
-   return result;
+  
+function random(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
 }
 
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+dictionary = {};
 
-mass = [];  
-var i = 1;
-while (i<=wordsAmount) {
-	mass.push (generate(Math.ceil(Math.random() * (maxCharacter - minCharacter) + minCharacter)));           //заносим сгенерированые слова в массив
-	i++
+for (let i = 0; i < wordsAmount; i++) {
+var word = '';
+var letterAmount =  random(minCharacter, maxCharacter); 
+
+for (var j=0; j<letterAmount; j++) {
+  var charIndex = random(0, characters.length - 1);
+  word+= characters[charIndex];
 }
 
-var length = mass.filter(word => word.length == 1);
-
-    counter = 0;
-
-for (var key in length) {
-  counter++;
+if (dictionary[letterAmount]) {
+  dictionary[letterAmount].push(word);
+} else {
+  dictionary[letterAmount] = [];
+    dictionary[letterAmount].push(word);
 }
-if (counter!=0){
-console.log(counter+" words with 1 characters");}          //выводим пользователю слова с 1 символом и т.д.
-
-var length2 = mass.filter(word => word.length == 2);
-
-    counter = 0;
-
-for (var key in length2) {
-  counter++;
 }
-if (counter!=0){
-console.log(counter+" words with 2 characters");}
-
-var length3 = mass.filter(word => word.length == 3);
-
-    counter = 0;
-
-for (var key in length3) {
-  counter++;
+for (var key in dictionary) {
+  console.log('You have ' + dictionary[key].length + ' words with ' + key + ' length');
+  
 }
-if (counter!=0){
-console.log(counter+" words with 3 characters");}
 
-var length4 = mass.filter(word => word.length == 4);
-
-    counter = 0;
-
-for (var key in length4) {
-  counter++;
-}
-if (counter!=0){
-console.log(counter+" words with 4 characters");}
-
-var length5 = mass.filter(word => word.length == 5);
-
-    counter = 0;
-
-for (var key in length5) {
-  counter++;
-}
-if (counter!=0){
-console.log(counter+" words with 5 characters");}
-
-var length6 = mass.filter(word => word.length == 6);
-
-    counter = 0;
-
-for (var key in length6) {
-  counter++;
-}
-if (counter!=0){
-console.log(counter+" words with 6 characters");}
-
-var length7 = mass.filter(word => word.length == 7);
-
-    counter = 0;
-
-for (var key in length7) {
-  counter++;
-}
-if (counter!=0){
-console.log(counter+" words with 7 characters");}
-
-
-var sortMass = [];
-sortMass.push(length, length2, length3, length4, length5, length6, length7);   // создали новый массив и записываем в него слова с символами по возрастанию
-
-console.log(sortMass);
+console.log(dictionary);
