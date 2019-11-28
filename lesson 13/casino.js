dcfd7fcbe3b37313ae4626e1256939fa4d87b012
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-28 14:55:30 +0200

    Create casino.js

---
+++ lesson 13/casino.js
@@ -0,0 +1,33 @@
+var values = [1,2,3];
+
+var casino = {
+    lines: [],
+    result: null,
+    createRandomLineFrom: function (arr) {
+        return arr.map(function () {
+            return Math.ceil(Math.random() * (arr.length - 1));
+        });
+    },
+    fillLines: function (linesAmount, fromArray) {
+        var self = this;
+        this.lines = Array(linesAmount).fill(1).map(function () {
+            return self.createRandomLineFrom(fromArray);
+        })
+    },
+    checkLines: function () {
+        var lineIndex = (this.lines[0].length - 1) / 2,
+            value = this.lines[0][lineIndex];
+        return this.result = this.lines.every(function (line) {
+            return line[lineIndex] === value;
+        })
+    },
+
+ checkVertLines: function () {
+ 	for (var i=0; i<this.lines[0].length; i++) {
+if (this.lines[i][i] === this.lines[i][0] & this.lines[i][i] === this.lines[i][1] & this.lines[i][i] === this.lines[i][2] ) {
+	console.log (this.lines[i] + ' Совпадение!');}
+	else {console.log (this.lines[i] + ' Не совпало!');}
+	
+  }
+}
+}
