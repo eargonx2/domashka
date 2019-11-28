f04354660506f12142d2bde413254465e7ed3440
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-11-28 15:09:46 +0200

    Update script.js

--- lesson 13/script.js
+++ lesson 13/script.js
@@ -2,4 +2,7 @@ casino.fillLines(3, values);
 
 var result = casino.checkLines();
 console.log(casino.lines);
+console.log('Сравниваем по строке:')
 console.log(casino.checkVertLines());
+console.log('Сравниваем по колонке (первая надпись = первая колонка):')
+console.log(casino.checkHorizLines());
