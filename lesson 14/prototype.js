5f7690d2c36ca2180da394ead703a57e74b46e77
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-12-05 02:18:17 +0200

    Update prototype.js

--- lesson 14/prototype.js
+++ lesson 14/prototype.js
@@ -1,15 +1,10 @@
-function MilitaryResource(type, maxHealth, maxDistance) {
-this.type = type;
-this.maxHealth = this.health = maxHealth;
-this.maxDistance = this.distance = maxDistance;
+function MilitaryResource(type, health, distance) {
+   this.type = type;
+   this.health = maxHealth = health;
+   this.maxHealth = maxHealth;
+   this.distance = maxDistance = distance;
+   this.maxDistance = maxDistance;
 }
-
-MilitaryResource.prototype.isReadytoMove = function () {
-return this.distance >0;
-}
-
-var resource = new MilitaryResource('warrior', 200, 500);
-console.log(resource, resource.isReadytoMove());
 
 
 MilitaryResource.prototype.isReadyToMove;
@@ -31,14 +26,45 @@ Squad.prototype.combineResources;
 Squad.prototype.cloneResource;
 
 
-var mag = new MilitaryResource('magician', 300, 700);
-console.log(mag, mag.isReadytoMove());
+var mag = new MilitaryResource('magician', 200,300);  // создаем нового юнита
 
+
+MilitaryResource.prototype.isReadytoMove = function () {
+return this.distance <= this.maxDistance;
+}
+console.log(mag.isReadytoMove());
 MilitaryResource.prototype.isReadyToFight = function () {
 return this.health > 0;
 }
-
 console.log(mag.isReadyToFight());
+MilitaryResource.prototype.restore = function () {
+return this.health = this.maxHealth,
+       this.distance = this.maxDistance;
+}
+
+mag.health = 20;  // занижаем hp
+mag.distance = 12; // занижаем distance
+console.log(mag);
+
+mag.restore();  // восстанавливаем hp и distance
+console.log(mag);
+
+
+MilitaryResource.prototype.clone = function () {
+return new MilitaryResource(this.type, this.health, this.maxHealth, this.distance, this.maxDistance);
+}
+
+var mg = mag.clone();  //клонируем юнит в новую переменную
+console.log(mg);
+
 
 var party = new Squad();
+
+// Squad.prototype.isReadyToMove = function () {
+//   return this.distance > 0;
+// }
+
+// Squad.prototype.isReadyToFight = function () {
+//   return this.health > 0;
+// }
 console.log(party);
