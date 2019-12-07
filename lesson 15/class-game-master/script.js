c61e247f414071585b6168ae9b69218daa340bd1
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-12-07 16:20:09 +0200

    Add files via upload

---
+++ lesson 15/class-game-master/script.js
@@ -0,0 +1,17 @@
+var croco = new CrocoTurtle(50);
+var bear = new MonsterBear(2);
+var dog = new WildDog(3);
+var dragon = new Dragon(30);
+var yeti = new Yeti(4);
+var boar = new WildBoar(13);
+var gigant = new Gigant(40);
+var player = new Player(1);
+
+croco.attack();
+bear.attack();
+dog.attack();
+dragon.attack();
+yeti.attack();
+boar.attack();
+gigant.attack();
+player.attack();
