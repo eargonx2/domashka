4da2b5a31574c06933a0f4fd0ce234cc9e87b03c
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-12-12 19:21:38 +0200

    Update squad.js

--- lesson 16/squad.js
+++ lesson 16/squad.js
@@ -1,34 +1,34 @@
 var Squad = (function () {
-    function Squad(defaultResources) {
-        this.squad = [];
-        if (defaultResources) this.combineResources(defaultResources);
-    }
+  function Squad(defaultResources) {
+    this.squad = [];
+    if (defaultResources) this.combineResources(defaultResources);
+  }
 
 
-    Squad.prototype.isReadyToMove = function () {
-        return this.squad.some((obj) => (obj.health > 0 && obj.distance > 0));
-    }
+  Squad.prototype.isReadyToMove = function () {
+    return this.squad.some((obj) => (obj.health > 0 && obj.distance > 0));
+  }
 
-    Squad.prototype.isReadyToFight = function () {
-        return this.squad.some((obj) => (obj.health > 0));
-    }
+  Squad.prototype.isReadyToFight = function () {
+    return this.squad.some((obj) => (obj.health > 0));
+  }
 
-    Squad.prototype.restore = function () {
-        squad.MilitaryResource = new MilitaryResource(this.type, this.health, this.maxHealth, this.distance, this.maxDistance);
-    }
+  Squad.prototype.restore = function () {
+    this.squad.forEach(element => {
+      element.health = maxHealth;
+      element.distance = maxDistance;
+    });
+  }
 
-    Squad.prototype.getReadyToMoveResources = function () {
-        return this.squad.some((obj) => (obj.health > 0 && obj.distance > 0));
-    }
-    Squad.prototype.combineResources = function (defaultResources) {
-        this.squad = defaultResources;
-    }
-    return Squad;
+  Squad.prototype.getReadyToMoveResources = function () {
+    return this.squad.some((obj) => (obj.health > 0 && obj.distance > 0));
+  }
+  Squad.prototype.combineResources = function (defaultResources) {
+    this.squad = defaultResources;
+  }
+  Squad.prototype.cloneResources = function() {
+    return new Squad(this.squad);
+  }
+  return Squad;
 }());
-
-var squad = new Squad([mag, mag2]);
-console.log(squad.squad);
-console.log(squad.isReadytoMove());
-console.log(squad.restore());
-
 
