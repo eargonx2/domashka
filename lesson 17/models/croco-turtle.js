132f4f0fabfec2bbe6c6beef83c83d6c7d7bcff5
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-12-15 19:02:20 +0200

    Create croco-turtle.js

---
+++ lesson 17/models/croco-turtle.js
@@ -0,0 +1,14 @@
+var CrocoTurtle = (function () {
+    function CrocoTurtle(level) {
+        Unit.call(this, 100, 400, level);
+    }
+
+    CrocoTurtle.prototype = Object.create(Unit.prototype);
+    CrocoTurtle.prototype.constructor = CrocoTurtle;
+
+    CrocoTurtle.prototype.step = function() {
+        console.log('Step');
+    };
+
+    return CrocoTurtle;
+}());
