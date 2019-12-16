6ef561bc83b3e158d6bbbf817c21d29407da0132
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2019-12-16 19:22:12 +0200

    Update script.js

--- lesson 17/script.js
+++ lesson 17/script.js
@@ -2,9 +2,6 @@ var croco = new CrocoTurtle(50);
 var bear = new MonsterBear(2);
 var gig = new Gigant(2);
 console.log(bear);
-croco.attack();
-bear.attack();
-
 console.log(bear);
 
 bear.attack(gig);
