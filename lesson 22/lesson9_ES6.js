b346e2dfb93e116c3a2cc8b5e0b98d33fddb9d9c
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-04 14:44:08 +0200

    Create lesson9_ES6.js

---
+++ lesson 22/lesson9_ES6.js
@@ -0,0 +1,11 @@
+{
+const mass = ['adsd','basas','casas','casas'];
+let newMass = [];
+
+mass.forEach(function(item, i, mass) {
+// i++; если надо значение строки начинать с 1, а не с 0
+  return newMass.push( i + ':' + item);
+});
+console.log(mass);
+console.log(newMass);
+}
