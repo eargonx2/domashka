07e91391688da16fa622570089237f2724e27308
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-09 20:18:47 +0200

    Update filter.js

--- lesson 11/filter.js
+++ lesson 11/filter.js
@@ -1,8 +1,7 @@
 var arr = [1, -1, 2, -2, 3];
-var newArr = [];
 
 function filter(arr, cb) {
-  if (!cb) return;
+  var newArr = [];
   for (var i=0; i < arr.length; i++) {
     if(cb(arr[i], i, arr)) {
        newArr.push(arr[i]);
