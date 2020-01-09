ca2d5707791895df89ad99e42a8feb87330926a4
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-09 20:19:37 +0200

    Update some.js

--- lesson 11/some.js
+++ lesson 11/some.js
@@ -1,8 +1,7 @@
 var arr = [1, -1, 2, -2, 3];
 
 function some(arr, cb) {
-  if (!cb) return;
-  for (var i=0; i < arr.length; i++) {
+    for (var i=0; i < arr.length; i++) {
     if(cb(arr[i], i, arr)) {
       return true;
     }
