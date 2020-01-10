15124eca918ece9b71a0a6fd79c91c8f3c350dfc
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-10 18:25:01 +0200

    Update lesson11_es6.js

--- lesson 22/lesson11_es6.js
+++ lesson 22/lesson11_es6.js
@@ -1,3 +1,7 @@
+
+////////////Some//////////////////////////
+
+
 function some(name, lastname, marks) {
     console.log(name);
     console.log(lastname);
@@ -13,3 +17,18 @@ function some(name, lastname, ...marks) {
 }
 
 some('Vasya', 'Pupkin', 5, 6, 12);
+
+
+
+
+////////////метод Filter, показывает значения больше 0//////////////////////////
+
+function forEach(arr, cb = function () { }) {
+    for (let i = 0; i < arr.length; i++) {
+        cb();
+        if (arr[i] > 0) {
+            console.log(arr[i])
+        }
+    }
+}
+forEach([-1, -2, 0, 4]);
