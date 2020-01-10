f40900a939b5d034d206984140eb4393e39c3cc0
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-10 18:19:51 +0200

    Update lesson9_ES6.js

--- lesson 22/lesson9_ES6.js
+++ lesson 22/lesson9_ES6.js
@@ -1,11 +1,26 @@
+////////////Some//////////////////////////
 {
-const mass = ['adsd','basas','casas','casas'];
-let newMass = [];
+    const mass = ['adsd', 'basas', 'casas', 'casas'];
+    let newMass = [];
 
-mass.forEach(function(item, i, mass) {
-// i++; если надо значение строки начинать с 1, а не с 0
-  return newMass.push( i + ':' + item);
-});
-console.log(mass);
-console.log(newMass);
+    mass.forEach(function (item, i, mass) {
+        // i++; если надо значение строки начинать с 1, а не с 0
+        return newMass.push(i + ':' + item);
+    });
+    console.log(mass);
+    console.log(newMass);
 }
+
+
+
+////////////метод Filter, показывает значения больше 0//////////////////////////
+
+function forEach(arr, cb = function () { }) {
+    for (let i = 0; i < arr.length; i++) {
+        cb();
+        if (arr[i] > 0) {
+            console.log(arr[i])
+        }
+    }
+}
+forEach([-1, -2, 0, 4]);
