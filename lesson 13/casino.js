22ba9eaa639299c0bf6cf98eeb06cf791f8b5dda
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-12 11:40:12 +0200

    Update casino.js

--- lesson 13/casino.js
+++ lesson 13/casino.js
@@ -1,42 +1,39 @@
-var values = [1,2,3];
-
-var casino = {
-    lines: [],
-    result: null,
-    createRandomLineFrom: function (arr) {
-        return arr.map(function () {
-            return Math.ceil(Math.random() * (arr.length - 1));
-        });
-    },
-    fillLines: function (linesAmount, fromArray) {
-        var self = this;
-        this.lines = Array(linesAmount).fill(1).map(function () {
-            return self.createRandomLineFrom(fromArray);
-        })
-    },
-    checkLines: function () {
-        var lineIndex = (this.lines[0].length - 1) / 2,
-            value = this.lines[0][lineIndex];
-        return this.result = this.lines.every(function (line) {
-            return line[lineIndex] === value;
-        })
-    },
-
- checkVertLines: function () {
- 	for (var i=0; i<this.lines[0].length; i++) {
-if (this.lines[i][i] === this.lines[i][0] & this.lines[i][i] === this.lines[i][1] & this.lines[i][i] === this.lines[i][2] ) {
-	console.log (this.lines[i] + ' Совпадение!');}
-	else {console.log (this.lines[i] + ' Не совпало!');}
-	
+var  values  =   [1,  2,  3];
+var  casino  =   {    
+  lines:  [],
+      result:  null,
+      createRandomLineFrom:   function (arr)  {        
+    return  arr.map(function ()  {            
+      return  Math.ceil(Math.random()  *  (arr.length  -  1));        
+    });    
+  },
+      fillLines:   function (linesAmount,  fromArray)  {        
+    var  self  =  this;        
+    this.lines  =  Array(linesAmount).fill(1).map(function ()  {            
+      return  self.createRandomLineFrom(fromArray);        
+    })    
+  },
+      checkLines:   function ()  {        
+    var  lineIndex  =   (this.lines[0].length  -  1)  /  2,
+                  value  =  this.lines[0][lineIndex];        
+    return  this.result  =  this.lines.every(function (line)  {            
+      return  line[lineIndex]  ===  value;        
+    })    
+  },
+      checkHorizLines:   function ()  {           
+    for  (var  i  =  0;  i  <  this.lines.length;  i++)  {            
+      if  (this.lines[0][0] == this.lines[1][0] & this.lines[0][1] == this.lines[1][1] & this.lines[0][2] == this.lines[1][2])  {                  
+        return  true;   
+      }      
+    }    
+    return  false;
+  },
+      checkVertLines:   function ()  {        
+    for  (var  i  =  0;  i  <  this.lines.length;  i++)  {            
+      if  (this.lines[0][0] == this.lines[1][0] & this.lines[1][0] == this.lines[1][1] & this.lines[2][0] == this.lines[2][2])  {                  
+        return true;   
+      }      
+    }  
+    return  false;
   }
-},
-
- checkHorizLines: function () {
- 	for (var i=0; i<this.lines[0].length; i++) {
-if (this.lines[0][i] === this.lines[1][i] & this.lines[0][i] === this.lines[2][i]) {
-	console.log (this.lines[i] + ' Совпадение!');}
-	else {console.log (this.lines[i] + ' Не совпало!');}
-	
-  }
-}
 }
