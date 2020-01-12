12ff590553618592e7b28ef768d29fc29a980a1e
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-12 17:56:33 +0200

    Update filter.js

--- lesson 11/filter.js
+++ lesson 11/filter.js
@@ -1,16 +1,16 @@
 var arr = [1, -1, 2, -2, 3];
 
 function filter(arr, cb) {
-  var newArr = [];
-  for (var i=0; i < arr.length; i++) {
-    if(cb(arr[i], i, arr)) {
-       newArr.push(arr[i]);
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (cb(arr[i], i, arr)) {
+            newArr.push(arr[i]);
+        }
     }
-  }
-  return newArr;
+    return newArr;
 }
 
 
-console.log(filter(arr, function (el) {
-return el > 0;
+console.log(filter(arr, function(el) {
+    return el > 0;
 }));
