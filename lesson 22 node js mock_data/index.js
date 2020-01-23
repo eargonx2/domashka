665b795c8bc5a022d702fa912cc95299d3bdcb21
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-23 16:56:12 +0200

    Create index.js

---
+++ lesson 22 node js mock_data/index.js
@@ -0,0 +1,16 @@
+const { readFileWithPromise } = require("lib");
+const http = require("http");
+const hostname = "0.0.0.0";
+const port = 3000;
+const server = http.createServer((request, response) => {
+  readFileWithPromise("./MOCK_DATA.json")
+    .then(data => {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.end(data);
+    })
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
