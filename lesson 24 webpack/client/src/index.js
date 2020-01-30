6ad319e4571850685c1b8a62db4b714fb7780295
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-01-30 03:00:18 +0200

    Update index.js

--- lesson 24 webpack/client/src/index.js
+++ lesson 24 webpack/client/src/index.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
 const { apiUrl } = require("./constants");
-axios.get("https://swapi.co/api/planets/3/").then(data => console.log(data));
+axios.get({apiUrl}).then(data => console.log(data));
 
