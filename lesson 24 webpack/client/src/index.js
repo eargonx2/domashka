80f3aa32be67dceece9f92ecca30cd75421c1c30
eargonx2 <57111403+eargonx2@users.noreply.github.com>
2020-02-06 22:54:50 +0200

    Update index.js

--- lesson 24 webpack/client/src/index.js
+++ lesson 24 webpack/client/src/index.js
@@ -2,3 +2,27 @@ const axios = require('axios');
 const { apiUrl } = require("./constants");
 axios.get(`${apiUrl}`).then(data => console.log(data));
 
+function getApi() {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://swapi.co/api/planets/3/", true);
+
+  xhr.send();
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState != 4) return;
+
+    button.innerHTML = "Готово!";
+
+    if (xhr.status != 200) {
+      // обработать ошибку
+      alert(xhr.status + ": " + xhr.statusText);
+    } else {
+      // вывести результат
+      alert(xhr.responseText);
+    }
+  };
+
+  button.innerHTML = "Загружаю...";
+  button.disabled = true;
+}
